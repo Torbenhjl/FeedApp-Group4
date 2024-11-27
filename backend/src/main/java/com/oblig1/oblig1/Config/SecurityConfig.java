25276796345123e832760270ccd807071623c3ab
Fredrik Enes <594754@stud.hvl.no>
2024-11-27 12:20:16 +0100

    test

--- backend/src/main/java/com/oblig1/oblig1/Config/SecurityConfig.java
+++ backend/src/main/java/com/oblig1/oblig1/Config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.oblig1.oblig1.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,6 +21,9 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
 
 
     @Bean
@@ -24,27 +31,25 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/helloUser").hasRole("user")  // Role must match
+                        .requestMatchers("/login**", "/oauth2/**").permitAll()
+                        .requestMatchers("/helloUser").hasAuthority("ROLE_user")  // Role must match
                         .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt
-                        .jwtAuthenticationConverter(jwtAuthenticationConverter())  // Map roles
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")  // Keycloak will automatically redirect here for authentication
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter()).decoder(jwtDecoder())  // Map roles
                 ))
                 .build();
     }
 
     @Bean
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");  // Add "ROLE_" prefix for Spring Security
-        grantedAuthoritiesConverter.setAuthoritiesClaimName("resource_access.springboot-client.roles"); // Extract roles
-
-        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-        converter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
-        return converter;
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder
+                .withJwkSetUri(jwkSetUri)
+                .jwsAlgorithm(SignatureAlgorithm.RS256).build();
     }
-
-
-
 
 }
